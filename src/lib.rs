f1828b2c3ead78faf49b31bdc61dac24923cd41c
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-07-14 11:03:36 -0400

    stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,35 @@
 pub mod dict {
-    pub struct Dict<K, V> {
+    pub struct Dict<K: Eq, V> {
         table: Vec<(K, V)>
     } 
 
-    impl<K: std::cmp::PartialEq, V> Dict<K, V> {
+    impl<K: Eq, V> Dict<K, V> {
         pub fn new() -> Self {
             Self { table: vec![] }
         }
         
         pub fn set(&mut self, key: K, value: V) {
-            self.table.push((key, value));
+            match self.find_key(&key) {
+                Some(idx) => self.table[idx] = (key, value),
+                None => self.table.push((key, value))
+            }
         }
 
         pub fn get(&self, item: &K) -> Option<&V> {
+            match self.find_key(item) {
+                Some(idx) => Some(&self.table[idx].1),
+                None => None
+            }
+        }
+
+        fn find_key(&self, item: &K) -> Option<usize> {
             self.table
                 .iter()
-                .filter_map(|(key, value)| (*key == *item).then(|| value))
-                .last()
+                .position(|(key, _)| *key == *item)
         }
     }
 
-    impl<K: std::cmp::PartialEq, V : std::fmt::Display> Dict<K, V> {
+    impl<K: Eq, V : std::fmt::Display> Dict<K, V> {
         pub fn print(&self, item: &K) {
             match self.get(item) {
                 None => println!("key not found"),
