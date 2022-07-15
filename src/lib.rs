6e5ba7c65492e135ffcce6223e1acb8fdd02a922
Avery R <contact@nekodjin.org>
2022-07-15 16:44:58 +0100

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,32 @@
 pub mod dict {
-    use std::{hash::{Hasher, Hash}, collections::hash_map::DefaultHasher};
+    use std::{
+        collections::hash_map::DefaultHasher,
+        hash::{Hash, Hasher},
+    };
 
     pub struct Dict<V> {
-        table: Vec<Option<V>>
-    } 
+        table: Vec<Option<V>>,
+    }
 
     impl<V> Dict<V> {
         pub fn new(n: usize) -> Self {
-            Self { table: std::iter::repeat_with(|| None).take(n).collect() }
+            Self {
+                table: std::iter::repeat_with(|| None).take(n).collect(),
+            }
         }
-        
+
         pub fn set<K: Hash>(&mut self, key: K, value: V) {
             let hashed = self.hash_key(&key);
             match self.table.get(hashed) {
                 Some(_) => self.table[hashed] = Some(value),
-                None => self.table[hashed] = Some(value)
+                None => self.table[hashed] = Some(value),
             }
         }
 
         pub fn get<K: Hash>(&self, key: &K) -> Option<&V> {
             match self.table.get(self.hash_key(key)) {
-                Some(Some(value)) => Some(&value), 
-                Some(None) | None => None
+                Some(Some(value)) => Some(&value),
+                Some(None) | None => None,
             }
         }
 
@@ -36,9 +41,8 @@ pub mod dict {
         pub fn print<K: Hash>(&self, item: &K) {
             match self.get(item) {
                 None => println!("key not found"),
-                Some(value) => println!("{}", *value)
+                Some(value) => println!("{}", *value),
             }
         }
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use dict_rs::dict::*;
 
 fn main() {
     let mut dict: Dict<i32> = Dict::new(1000);
+
     dict.set("rust", 15);
     dict.set(1234, 32);
     dict.set("hi", 1);
     dict.set([1, 2, 3], 78);
     dict.print(&"hi");
-    dict.print(&1234); 
+    dict.print(&1234);
     dict.print(&"bad key");
     dict.print(&[1, 2, 3])
 }
